0f51a3fa24aec1912b60e61ca7725964106ef79c
valeriacruise <valeriacruise22@gmail.com>
2018-07-10 14:18:05 +0000

    automatically backed up by learn

--- name.rb
+++ name.rb
@@ -1,6 +1,6 @@
-def say_hello(your_name,my_name="ruby")
+def say_hello(your_name, my_name="ruby")
   puts "Hello #{your_name}!"
   puts "It is 10:10 in the morning on Tuesday, July 10th."
 end
 
-say_hello("leila")
+say_hello("leila", "ruby")
