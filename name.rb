f0f1f838d2c008166898e73f13d964851cb172b4
valeriacruise <valeriacruise22@gmail.com>
2018-07-10 14:07:03 +0000

    automatically backed up by learn

--- name.rb
+++ name.rb
@@ -1,6 +1,6 @@
 def say_hello(your_name)
   puts "Hello #{your_name}!"
-  puts "It is 10:10 in the morning on Tuesday, July 10th!"
+  puts "It is 10:10 in the morning on Tuesday, July 10th."
 end
 
 say_hello("leila")
